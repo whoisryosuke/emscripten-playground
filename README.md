757659e3a72275bdd382906798e11813b64a10ea
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-11-02 11:26:55 -0700

    :tada: basic emscripten examples

---
+++ README.md
@@ -0,0 +1,16 @@
+# Emscripten Playground
+
+Examples of using Emscripten to compile C++ to web.
+
+## Examples
+
+- Hello World Console
+- Exporting Functions to JS
+- OpenGL Rendering
+- Exporting OpenGL Functions
+
+## Getting Started
+
+1. Install Emscripten and make sure `emcc` binary is available in your shell. You can run `emcc -v` as a test.
+1. `git clone`
+1. Check the `.c` or `.cpp` file for comment on top with a `emcc` script to run and compile.
---
+++ examples/opengl-test2/opengl-test.cpp
@@ -0,0 +1,115 @@
+// emcc -o opengl-test.html opengl-test.cpp -s USE_SDL=2 -s FULL_ES2=1 -s SINGLE_FILE=1
+// emcc -o opengl-test-asyncify.html opengl-test.cpp -s USE_SDL=2 -s FULL_ES2=1 -s ASYNCIFY=1 -s SINGLE_FILE=1
+
+#include <exception>
+#include <functional>
+
+#define GL_GLEXT_PROTOTYPES 1
+
+#include <emscripten.h>
+#include <SDL/SDL.h>
+#include <SDL_opengles2.h>
+
+// Shader sources
+const GLchar* vertexSource =
+    "attribute vec4 position;                      \n"
+    "varying vec3 color;                           \n"
+    "void main()                                   \n"
+    "{                                             \n"
+    "    gl_Position = vec4(position.xyz, 1.0);    \n"
+    "    color = gl_Position.xyz + vec3(0.5);      \n"
+    "}                                             \n";
+
+// Fragment/pixel shader
+const GLchar* fragmentSource =
+    "precision mediump float;                     \n"
+    "varying vec3 color;                          \n"
+    "void main()                                  \n"
+    "{                                            \n"
+    "    gl_FragColor = vec4 ( color, 1.0 );      \n"
+    "}                                            \n";
+
+
+std::function<void()> loop;
+void main_loop() { loop(); }
+
+int main(int argc, char** argv)
+{
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+    auto wnd(
+        SDL_CreateWindow("test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+            640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN));
+
+    auto glc = SDL_GL_CreateContext(wnd);
+
+    SDL_GL_SetSwapInterval(0);
+
+    auto rdr = SDL_CreateRenderer(
+        wnd, -1, SDL_RENDERER_ACCELERATED);
+
+    // Create Vertex Array Object
+    GLuint vao;
+    glGenVertexArraysOES(1, &vao);
+    glBindVertexArrayOES(vao);
+
+    // Create a Vertex Buffer Object and copy the vertex data to it
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+
+    GLfloat vertices[] = {0.0f, 0.5f, 0.5f, -0.5f, -0.5f, -0.5f};
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Create and compile the vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Create and compile the fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Link the vertex and fragment shader into a shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    // Specify the layout of the vertex data
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glEnableVertexAttribArray(posAttrib);
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+    loop = [&]
+    {
+        SDL_Event e;
+        while(SDL_PollEvent(&e))
+        {
+            if(e.type == SDL_QUIT) std::terminate();
+        }
+
+        // Clear the screen to black
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw a triangle from the 3 vertices
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        SDL_GL_SwapWindow(wnd);
+    };
+
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(main_loop, 0, true);
+#else
+    while(true) main_loop();
+#endif
+
+    return 0;
+}
